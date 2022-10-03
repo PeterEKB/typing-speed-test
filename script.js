1f03081f47c453e218b1f0fc3fabb147ac65e15d
PeterEKB <109247624+PeterEKB@users.noreply.github.com>
2022-10-02 22:52:10 -0500

    1 file modified

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 /* * * * APIs * * * */
 class APIs {
-  rndWord = async (c,l) => {
+  rndWord = async (c, l) => {
     const bUrl = 'https://random-word-api.herokuapp.com/word',
       query = c ? (l ? `?number=${c}&length=${l}` : `?number=${c}`) : '';
 
@@ -12,9 +12,9 @@ class APIs {
 
 class View {
   typeArea = document.querySelector('type-area');
-  time = document.querySelector('time')
-  speed = document.querySelector('speed')
-  accuracy = document.querySelector('accuracy')
+  time = document.querySelector('time');
+  speed = document.querySelector('speed');
+  accuracy = document.querySelector('accuracy');
   inputCon = this.typeArea.querySelector('input-con');
   txtDis = this.typeArea.querySelector('textarea-con');
   txtRec = this.typeArea.querySelector('commit');
@@ -25,9 +25,9 @@ class View {
 /* * * * Model * * * */
 
 class Model {
-  #time
-  time = 0
-  tLimit = 60
+  #time;
+  time = 0;
+  tLimit = 60;
   isActive = 0;
   isEnvokable = 1;
   pos = 0;
@@ -38,8 +38,8 @@ class Model {
     accuracy: '',
     accuracyInt: 0,
     speed: '',
-    speedInt: 0
-  }
+    speedInt: 0,
+  };
   words = [];
   typed = '';
   committed = [];
@@ -48,18 +48,18 @@ class Model {
     this.view = data.Views || data.View || data.views || data.view;
     this.#listeners;
   }
-  
+
   async getWords(c, l) {
     let promise = this.apis.rndWord(c, l);
-    
+
     this.words = await promise;
-    
+
     return this.words;
   }
-  
-  get reset(){
-    this.timer = 0
-    this.time = 0
+
+  get reset() {
+    this.timer = 0;
+    this.time = 0;
     this.isActive = 0;
     this.isEnvokable = 1;
     this.pos = 0;
@@ -72,33 +72,32 @@ class Model {
       accuracy: '',
       accuracyInt: 0,
       speed: '',
-      speedInt: 0
-    }
+      speedInt: 0,
+    };
     this.typed = '';
     this.committed = [];
-    this.view.txtRec.innerHTML = ''
-    this.view.txtDis.innerHTML = ''
-    this.disTyped
-    App.init
+    this.view.txtRec.innerHTML = '';
+    this.view.txtDis.innerHTML = '';
+    this.disTyped;
+    App.init;
   }
-  get timer(){
-    return this.time
+  get timer() {
+    return this.time;
   }
-  set timer(p){
-    let n = 1
+  set timer(p) {
+    let n = 1;
 
-    if(p){
-      this.#time = setInterval(()=>{
-        this.time = +(parseFloat(this.time) + parseFloat(n)).toFixed(2)
-        if(this.time === this.tLimit)
-        this.timer = 0
-        this.view.time.innerHTML = this.time
-        this.calcScore()
-        this.view.speed.innerHTML = this.score.speedInt
-        this.view.accuracy.innerHTML = this.score.accuracy
-      },n*1000)
-    }else{
-      this.end
+    if (p) {
+      this.#time = setInterval(() => {
+        this.time = +(parseFloat(this.time) + parseFloat(n)).toFixed(2);
+        if (this.time === this.tLimit) this.timer = 0;
+        this.view.time.innerHTML = this.time;
+        this.calcScore();
+        this.view.speed.innerHTML = this.score.speedInt;
+        this.view.accuracy.innerHTML = this.score.accuracy;
+      }, n * 1000);
+    } else {
+      this.end;
     }
   }
 
@@ -114,44 +113,43 @@ class Model {
   set remLast(t) {
     this.typed = this.typed.slice(0, -t);
   }
-  format(t,d) {
+  format(t, d) {
     const ele = document.createElement('text'),
       txt = document.createTextNode(t);
     ele.appendChild(txt);
-    if(typeof(d) !== 'undefined'){
-      const s = d === false? 'wrong': 'correct'
-      ele.classList.add(s)
+    if (typeof d !== 'undefined') {
+      const s = d === false ? 'wrong' : 'correct';
+      ele.classList.add(s);
     }
     return ele;
   }
   calcScore(r) {
-    const s = this.score
+    const s = this.score;
     if (r === true) {
-      s.correct++
-      s.total++
+      s.correct++;
+      s.total++;
     } else if (r === false) {
-      s.incorrect++
-      s.total++
+      s.incorrect++;
+      s.total++;
     }
-    s.accuracyInt = (s.correct / s.total * 100).toFixed(0)
-    s.accuracy = s.accuracyInt + '%'
-    s.speedInt = (s.total * 60 / this.time).toFixed(0)
-    s.speed = s.speedInt + ' words per minute'
-    console.log(r, s.speed)
-    return s
+    s.accuracyInt = ((s.correct / s.total) * 100).toFixed(0);
+    s.accuracy = s.accuracyInt + '%';
+    s.speedInt = ((s.total * 60) / this.time).toFixed(0);
+    s.speed = s.speedInt + ' words per minute';
+    console.log(r, s.speed);
+    return s;
   }
   get commit() {
     const typed = this.typed,
       same = this.compare;
-    this.calcScore(same)
+    this.calcScore(same);
     this.committed.push(typed);
     this.shift;
     this.typed = '';
     return { typed, same };
   }
   get compare() {
-    if (this.typed === this.words[this.pos]) 
-      return true;
+    if (this.typed === this.words[this.pos]) return true;
     return false;
   }
   get shift() {
@@ -174,7 +172,6 @@ class Model {
   get disTyped() {
     this.view.txtTrack.innerHTML = this.typed;
   }
-  display()
   disWords(c) {
     const tA = this.view.txtDis;
     c.forEach((v) => {
@@ -184,40 +181,36 @@ class Model {
   }
   set pushTyped(t) {
     const ele =
-      typeof(t) === 'object'
-        ? this.format(t.typed, t.same)
-        : this.format(t);
+      typeof t === 'object' ? this.format(t.typed, t.same) : this.format(t);
     this.view.txtRec.appendChild(ele);
   }
-  set activate(s){
+  set activate(s) {
     const v = this.view;
-    
-    if(s){
-      v.typeArea.classList.add(v.active)
-      this.isActive = 1
-    }else{
-      v.typeArea.classList.remove(v.active)
+
+    if (s) {
+      v.typeArea.classList.add(v.active);
+      this.isActive = 1;
+    } else {
+      v.typeArea.classList.remove(v.active);
       this.isActive = 0;
     }
   }
-  get end(){
-    this.activate = 0
-    this.isEnvokable = 0
-    clearInterval(this.#time)
-    this.#time = undefined
+  get end() {
+    this.activate = 0;
+    this.isEnvokable = 0;
+    clearInterval(this.#time);
+    this.#time = undefined;
   }
   get #listeners() {
     const v = this.view;
-    
-    window.addEventListener('blur', (e) => {
-    });
+
+    window.addEventListener('blur', (e) => {});
     document.addEventListener('click', (e) => {
       if (e.path.includes(v.typeArea)) {
-        this.activate = 1
-        if(!this.isEnvokable)
-          this.reset
+        this.activate = 1;
+        if (!this.isEnvokable) this.reset;
       } else {
-        this.activate = 0
+        this.activate = 0;
       }
     });
     document.addEventListener('keydown', (e) => {
@@ -225,8 +218,7 @@ class Model {
       if (this.isActive && this.isEnvokable) {
         if (t.length === 1 && t !== ' ') {
           this.trackTyped = e.key.toLowerCase();
-          if(!this.#time)
-          this.timer = 1
+          if (!this.#time) this.timer = 1;
         } else {
           const comm = () => {
             if (this.typed !== '') {
@@ -252,7 +244,7 @@ class Model {
               break;
             case 'control':
               e.preventDefault();
-              this.end
+              this.end;
               console.log(t, 'cannot be logged');
               break;
             default:
@@ -279,7 +271,6 @@ class Controller {
     wrds.then((res) => {
       this.showModel = res;
     });
-    
   }
 
   set showModel(s) {
